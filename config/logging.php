eac4b241b12f92a88f3fbdc9974b8b96facc0af6
Alex <alex_kir@live.nl>
2019-06-02 11:09:33 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -51,7 +51,6 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
-            'permission' => 0664,
         ],
 
         'slack' => [
